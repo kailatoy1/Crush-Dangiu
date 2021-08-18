37a6a70d6250ecf6c39b7178fb1df621fdce1a3a
kailatoy1 <89116228+kailatoy1@users.noreply.github.com>
2021-08-18 10:18:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Do anh Kai đẹp zaii <33333333',
+    reply: 'Anh Kai đepzai quá sao hong iu được <3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Vậy mình hẹn hò nhaaa ><.',
     btnAccept: 'Okiiiii lun <3',
